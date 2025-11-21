08bef30650fa429c42e9fbaeb0570574d00521f3
GRicciardi00 <ricciardi.giuseppe00gmail.com>
2025-11-21 14:25:32 +0100

    Removed image_size from SwinUNETR as is not supported from Monai >1.3.3

--- swincell/train_main.py
+++ swincell/train_main.py
@@ -128,7 +128,6 @@ def main_worker(gpu, args):
         )
     elif str.lower(args.model)  =='swin':
         model = SwinUNETR(
-            img_size=(args.roi_x, args.roi_y, args.roi_z),
             in_channels=args.in_channels,
             out_channels=args.out_channels,
             feature_size=args.feature_size,
